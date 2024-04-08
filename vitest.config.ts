7ec6d8b2eba3c230dd70212e8236ab11596b2906
ntnirajthakur21 <ntnirajthakur21@gmail.com>
2024-04-09 00:53:52 +0545

    Add vitest/globals to tsconfig.json types Add tsconfig.node.json for Node.js configuration Update vitest.config.ts for testing environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     clearMocks: true,
+    globals: true,
+    environment: "node",
+    // setupFiles: ["./setupTests.ts"],
   },
 });
